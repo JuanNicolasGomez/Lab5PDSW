2e5b091b9c829160d3ffc4ec6b0fa0fffb6e7061
JonathanCuestaSanchez <jonathancuesta97@gmail.com>
2017-09-21 12:18:53 -0500

    creacion de prueba de registro de consultas

--- src/test/java/tests/ServiciosPacientesTest.java
+++ src/test/java/tests/ServiciosPacientesTest.java
@@ -5,12 +5,23 @@
  */
 package tests;
 
+import edu.eci.pdsw.samples.entities.Consulta;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
+import edu.eci.pdsw.samples.services.impl.ServiciosPacientesMock;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Diseño de pruebas 
+ * 
+ * Clases de equivalencia:
+ *     
+ *      CE1:paciente no esta registrado
+ *          Resultado esperado: ExcepcionServiciosPacientes
+ * 
+ *      CE2:paciente esta registrado
+ *          Resultado:no se lanza ninguna excepcion
  * @author hcadavid
  */
 public class ServiciosPacientesTest {
@@ -21,5 +32,12 @@ public class ServiciosPacientesTest {
     @Before
     public void setUp() {
     }
+    @Test (expected = ExcepcionServiciosPacientes.class)
+    public void testClaseDeEquivalencia1() throws ExcepcionServiciosPacientes{
+        Consulta consul=new Consulta ();
+        ServiciosPacientesMock servi= new ServiciosPacientesMock();
+        servi.agregarConsultaPaciente(5645464,"C.C",consul);
+    }
+    
     
 }
